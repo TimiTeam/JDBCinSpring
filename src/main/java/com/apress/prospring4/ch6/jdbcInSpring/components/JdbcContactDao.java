8df23be97af1619da4b8c0b34e7e9d55325dda92
Timur <timatiblackstar666@gmail.com>
2018-05-23 20:27:29 +0300

    Update the method FindFirstnameByID in Class JdbcContactDao

--- src/main/java/com/apress/prospring4/ch6/jdbcInSpring/components/JdbcContactDao.java
+++ src/main/java/com/apress/prospring4/ch6/jdbcInSpring/components/JdbcContactDao.java
@@ -4,21 +4,27 @@ package com.apress.prospring4.ch6.jdbcInSpring.components;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JdbcContactDao implements ContactDao,InitializingBean{
     private DataSource dataSource;
-    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate template;
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        NamedParameterJdbcTemplate template1 = new NamedParameterJdbcTemplate(dataSource);
+        this.template = template1;
     }
 
     public String findFirstNameById(Long id) {
-        return jdbcTemplate.queryForObject("SELECT first_name FROM contact WHERE id=?",
-                new Object[]{id},String.class);
+        String sql = "SELECT first_name FROM contact WHERE id = :contactId";
+        Map<String,Object> namedParameter = new HashMap<String, Object>();
+        namedParameter.put("contactId",id);
+        return template.queryForObject(sql,namedParameter,String.class);
     }
 
 
@@ -26,7 +32,7 @@ public class JdbcContactDao implements ContactDao,InitializingBean{
         if(dataSource == null){
             throw new BeanCreationException("Properties dataSource must be set!");
         }
-        if(jdbcTemplate == null){
+        if(template == null){
             throw new BeanCreationException("Null JdbcTemplate on ContactDao");
         }
     }
